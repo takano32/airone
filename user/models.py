b8a4f4690ac2585462f7cc6bbc8b60bc2dacfad3
Hiroyasu OHYAMA <oyama-hiroyasu@dmm.com>
2017-05-12 11:35:37 +0000

    Created models of AirOne

--- user/models.py
+++ user/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Member(models.Model):
+    name = models.CharField(max_length=200)
+    created_time = models.DateTimeField(auto_now=True)
+
+class User(Member):
+    userid = models.CharField(max_length=200)
+    passwd = models.CharField(max_length=200)
+    type = models.IntegerField(default=0)
+    logined_time = models.DateTimeField(auto_now=True)
+
+class Group(Member):
+    users = models.ManyToManyField(User)

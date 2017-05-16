1d579fed970e90fbea0742f61b2db57aa3319735
Hiroyasu OHYAMA <oyama-hiroyasu@dmm.com>
2017-05-16 03:53:25 +0000

    Implemented the operations for Users

---
+++ user/tests/test_view.py
@@ -0,0 +1,49 @@
+import json
+
+from django.test import TestCase, Client
+from django.urls import reverse
+from user.models import User
+from xml.etree import ElementTree
+
+
+class ViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_index(self):
+        resp = self.client.get(reverse('user:index'))
+        self.assertEqual(resp.status_code, 200)
+
+        root = ElementTree.fromstring(resp.content.decode('utf-8'))
+        self.assertIsNone(root.find('.//table'))
+
+    def test_index_with_user(self):
+        User(name='hoge').save()
+
+        resp = self.client.get(reverse('user:index'))
+        self.assertEqual(resp.status_code, 200)
+
+        root = ElementTree.fromstring(resp.content.decode('utf-8'))
+        self.assertIsNotNone(root.find('.//table'))
+        self.assertEqual(len(root.findall('.//tr')), 2)
+
+    def test_create_get(self):
+        resp = self.client.get(reverse('user:create'))
+        self.assertEqual(resp.status_code, 200)
+
+        root = ElementTree.fromstring(resp.content.decode('utf-8'))
+        self.assertIsNotNone(root.find('.//form'))
+
+    def test_create_post(self):
+        params = {
+            'name': 'hoge',
+            'userid': 'fuga',
+            'passwd': 'puyo',
+        }
+        resp = self.client.post(reverse('user:create'),
+                                json.dumps(params),
+                                'application/json')
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertIsNotNone(User.objects.first())
+        self.assertEqual(User.objects.first().name, 'hoge')

56f15ea69696962b5c9d6fbb7aa793cc10ff209f
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2017-05-16 03:53:25 +0000

    Implemented the operations for Users

---
+++ static/js/form.js
@@ -0,0 +1,27 @@
+$('form').submit(function(){
+  data = parseJson($('form').serializeArray());
+
+  $.ajax({
+    url:           $('form').attr('url'),
+    type:          'post',
+    dataType:      'json',
+    contentType:   'application/x-www-form-urlencoded;charset=utf-8',
+    scriptCharset: 'utf-8',
+    headers: {
+      'X-CSRFToken': $('input[name=csrfmiddlewaretoken]').val(),
+    },
+    data:          JSON.stringify(data)
+  });
+
+  location.reload();
+
+  return false;
+});
+
+var parseJson = function(data) {
+  var returnJson = {};
+  for (idx = 0; idx < data.length; idx++) {
+    returnJson[data[idx].name] = data[idx].value
+  }
+  return returnJson;
+}

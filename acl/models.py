53daaf1593f8263f91b376d8c33238d2666bb760
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2017-05-22 10:40:57 +0000

    Added implementations to add Entities and show them, but they don't care about ACL yet

--- acl/models.py
+++ acl/models.py
@@ -3,19 +3,6 @@ from user.models import Member
 
 
 class ACL(models.Model):
-    readable = models.ManyToManyField(Member, related_name='acl_readable')
-    writable = models.ManyToManyField(Member, related_name='acl_writable')
-    deletable = models.ManyToManyField(Member, related_name='acl_deletable')
-
-class ACLBaseModel(models.Model):
-    acl = models.ForeignKey(ACL)
-
-    def __init__(self, **kwargs):
-        super(ACLBaseModel, self).__init__(**kwargs)
-
-        # create a default ACL object
-        acl = ACL()
-        acl.save()
-
-        # set embedded ACL object
-        self.acl = acl
+    readable = models.ManyToManyField(Member, related_name='acl_readable', blank=True)
+    writable = models.ManyToManyField(Member, related_name='acl_writable', blank=True)
+    deletable = models.ManyToManyField(Member, related_name='acl_deletable', blank=True)

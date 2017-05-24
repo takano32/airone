6e46d6273c1bd5378a390ffca64fc542e5e50279
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2017-05-24 03:18:50 +0000

    Refactored Entity and AttributeBase model about ACL to simplify the ACL view implementation

--- acl/models.py
+++ acl/models.py
@@ -6,3 +6,10 @@ class ACL(models.Model):
     readable = models.ManyToManyField(Member, related_name='acl_readable', blank=True)
     writable = models.ManyToManyField(Member, related_name='acl_writable', blank=True)
     deletable = models.ManyToManyField(Member, related_name='acl_deletable', blank=True)
+
+class ACLBase(models.Model):
+    name = models.CharField(max_length=200)
+    acl = models.ForeignKey(ACL, blank=True, null=True)
+
+    # This fields describes the sub-class of this object
+    objtype = models.IntegerField(default=0)

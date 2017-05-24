c61e5f161c42a8b9c23049e6a102ab67f1e7953c
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2017-05-24 10:51:55 +0900

    for staging environment

--- airone/wsgi.py
+++ airone/wsgi.py
@@ -9,6 +9,11 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 
 import os
 
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..' )
+from django.core.wsgi import get_wsgi_application
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "airone.settings")

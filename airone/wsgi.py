3d77c83bff570e2ea84a735b39a052c632ce9c1f
netcorp <netcorp@stg-airone01.dmm.co.jp>
2017-05-24 11:30:42 +0900

    remove duplicated statement

--- airone/wsgi.py
+++ airone/wsgi.py
@@ -8,11 +8,9 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..' )
-from django.core.wsgi import get_wsgi_application
 
 from django.core.wsgi import get_wsgi_application
 

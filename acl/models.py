1502ba22640f64d0971792fa799f0c95f8411f4d
Hiroyasu OHYAMA <oyama-hiroyasu@dmm.com>
2017-05-25 07:15:25 +0000

    Replaced 'lambda' to def statement for creating default ACL object in the ACLBase.
    Because Django has a restrictions that fails serialization processing
    when a model has Lambda (plz see below).
    https://docs.djangoproject.com/en/dev/topics/migrations/#serializing-values

--- acl/models.py
+++ acl/models.py
@@ -8,14 +8,14 @@ class ACL(models.Model):
     deletable = models.ManyToManyField(Member, related_name='acl_deletable', blank=True)
 
 class ACLBase(models.Model):
+    def _create_default_acl():
+        return ACL.objects.create()
+
     name = models.CharField(max_length=200)
-    acl = models.ForeignKey(ACL, default=lambda: ACL.objects.create())
+    acl = models.ForeignKey(ACL, default=_create_default_acl)
 
     # This fields describes the sub-class of this object
     objtype = models.IntegerField(default=0)
 
     def __init__(self, *args, **kwargs):
         super(ACLBase, self).__init__(*args, **kwargs)
-
-class ACLTest(models.Model):
-    test = models.ManyToManyField(Member, related_name='acl_test')

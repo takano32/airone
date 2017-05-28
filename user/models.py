7a5294e1f923b9c23f945a995c312de5b46b6222
Hiroyasu OHYAMA <oyama-hiroyasu@dmm.com>
2017-05-28 11:09:19 +0000

    Refactored 'user' application to utilize Django embedded data structure of 'User' and 'Group'

--- user/models.py
+++ user/models.py
@@ -1,12 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User as DjangoUser
 
 
-class Member(models.Model):
-    name = models.CharField(max_length=200)
-    created_time = models.DateTimeField(auto_now=True)
-
-class User(Member):
-    userid = models.CharField(max_length=200)
-    passwd = models.CharField(max_length=200)
-    type = models.IntegerField(default=0)
-    logined_time = models.DateTimeField(auto_now=True)
+class User(DjangoUser):
+    authorized_type = models.IntegerField(default=0)

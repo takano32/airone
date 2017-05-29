3128bf6bd6a095abcc3f1cc465f6fd943414b456
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2017-05-29 03:53:30 +0000

    Changed to save encrypted password in the database for Django restrictions

--- user/views.py
+++ user/views.py
@@ -18,20 +18,39 @@ def create(request):
     if request.method == 'POST':
         try:
             received_json = json.loads(request.body.decode('utf-8'))
-
-            if User.objects.filter(email=received_json['email']).count():
-                return HttpResponse('Specified Email address has been already registered',
-                                    status=400)
-
-            User(username=received_json['name'],
-                 password=received_json['passwd'],
-                 email=received_json['email']).save()
-
-            return render(request, 'user_create.html')
-        except KeyError:
-            return HttpResponse('Invalid parameters are specified', status=400)
         except json.decoder.JSONDecodeError:
             return HttpResponse('Failed to parse string to JSON', status=400)
 
-    else:
-        return render(request, 'user_create.html')
+        # validation check for the received data
+        if not _is_valid(received_json):
+            return HttpResponse('Invalid parameters are specified', status=400)
+
+        if User.objects.filter(email=received_json['email']).count():
+            return HttpResponse('Specified Email address has been already registered',
+                                status=400)
+
+        user = User(username=received_json['name'],
+                    email=received_json['email'])
+
+        # store encrypted password in the database
+        user.set_password(received_json['passwd'])
+        user.save()
+
+    return render(request, 'user_create.html')
+
+def _is_valid(params):
+    param_keys = ['name', 'email', 'passwd']
+
+    if not isinstance(params, dict):
+        return False
+    # These are existance checks of each parameters
+    if not all([(x in params) for x in param_keys]):
+        return False
+    # These are type checks of each parameters
+    if not all([isinstance(params[x], str) for x in param_keys]):
+        return False
+    # These are value checks of each parameters
+    if not all([params[x] for x in param_keys]):
+        return False
+
+    return True

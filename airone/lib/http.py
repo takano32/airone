44a9946e3319ca4b2ef438232a3e3278184c7f14
Hiroyasu OHYAMA <oyama-hiroyasu@dmm.com>
2017-06-01 03:08:35 +0000

    Made the pre-checking processing of HTTP GET/POST be generalized and reusable

--- airone/lib/http.py
+++ airone/lib/http.py
@@ -1,5 +1,65 @@
+import json
+
 from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 
 
 class HttpResponseSeeOther(HttpResponseRedirect):
     status_code = 303
+
+def http_get(func):
+    def wrapper(*args, **kwargs):
+        request = args[0]
+        if request.method != 'GET':
+            return HttpResponse('Invalid HTTP method is specified', status=400)
+
+        if not request.user.is_authenticated():
+            return HttpResponseSeeOther('/dashboard/login')
+
+        return func(*args, **kwargs)
+    return wrapper
+
+def http_post(validator):
+    def _decorator(func):
+        def http_post_handler(*args, **kwargs):
+            request = args[0]
+
+            if request.method != 'POST':
+                return HttpResponse('Invalid HTTP method is specified', status=400)
+
+            if not request.user.is_authenticated():
+                return HttpResponse('You have to login to execute this operation', status=401)
+
+            try:
+                kwargs['recv_data'] = json.loads(request.body.decode('utf-8'))
+            except json.decoder.JSONDecodeError:
+                return HttpResponse('Failed to parse string to JSON', status=401)
+
+            if not _is_valid(kwargs['recv_data'], validator):
+                return HttpResponse('Invalid parameters are specified', status=400)
+
+            return func(*args, **kwargs)
+        return http_post_handler
+    return _decorator
+
+def _is_valid(params, meta_info):
+    if not isinstance(params, dict):
+        return False
+    # These are existance checks of each parameters
+    if not all([x['name'] in params for x in meta_info]):
+        return False
+    # These are type checks of each parameters
+    if not all([isinstance(params[x['name']], x['type']) for x in meta_info]):
+        return False
+    # These are value checks of each parameters
+    for _meta in meta_info:
+        # The case specified value is str
+        if (_meta['type'] == str and 'checker' in _meta and not _meta['checker'](params)):
+            return False
+
+        # The case specified value is list
+        if (_meta['type'] == list and
+            not all([_is_valid(x , _meta['meta']) for x in params[_meta['name']]])):
+            return False
+
+    return True

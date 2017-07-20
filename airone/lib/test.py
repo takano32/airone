0fb8c53e020bae3210a43e45816ab7c1e3e4887b
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2017-07-20 01:43:00 +0000

    Implemented import processing for the Entity and AttributeBase

--- airone/lib/test.py
+++ airone/lib/test.py
@@ -1,3 +1,6 @@
+import inspect
+import os
+
 from django.test import TestCase, Client
 from user.models import User
 
@@ -15,3 +18,9 @@ class AironeViewTest(TestCase):
         self.client.login(username='admin', password='admin')
 
         return user
+
+    def open_fixture_file(self, fname):
+        test_file_path = inspect.getfile(self.__class__)
+        test_base_path = os.path.dirname(test_file_path)
+
+        return open("%s/fixtures/%s" % (test_base_path, fname), 'r')

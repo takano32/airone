b236d66b7e33844940628da8ed2f622155b0d44b
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2017-08-18 07:44:39 +0000

    Changed to be able to export AttributeValues typed ArrayString/ArrayObject

--- dashboard/tests/test_import.py
+++ dashboard/tests/test_import.py
@@ -2,6 +2,7 @@ import mock
 import re
 
 from airone.lib.test import AironeViewTest
+from airone.lib import types as atype
 from datetime import datetime
 from django.urls import reverse
 from entity.models import Entity, AttributeBase
@@ -18,11 +19,26 @@ class ImportTest(AironeViewTest):
         self.assertEqual(resp.status_code, 303)
         fp.close()
 
+        # checks each objects are created safety
         self.assertEqual(Entity.objects.count(), 3)
-        self.assertEqual(Entity.objects.get(name='foo').id, 1)
-        self.assertEqual(AttributeBase.objects.last().name, 'attr5')
-        self.assertEqual(AttributeBase.objects.last().type, 2)
-        self.assertEqual(Entity.objects.get(name='foo').attr_bases.count(), 3)
+        self.assertEqual(AttributeBase.objects.count(), 4)
+
+        # checks keeping the correspondence relationship with id and name
+        self.assertEqual(Entity.objects.get(id='1').name, 'entity1')
+        self.assertEqual(AttributeBase.objects.get(id='5').name, 'attr-obj')
+
+        # checks contains required attributes (for Entity)
+        entity = Entity.objects.get(name='entity')
+        self.assertEqual(entity.note, 'note1')
+
+        # checks contains required attributes (for AttributeBase)
+        self.assertEqual(entity.attr_bases.count(), 4)
+        self.assertEqual(entity.attr_bases.get(name='attr-str').type, atype.AttrTypeStr)
+        self.assertEqual(entity.attr_bases.get(name='attr-obj').type, atype.AttrTypeObj)
+        self.assertEqual(entity.attr_bases.get(name='attr-arr-str').type, atype.AttrTypeArrStr)
+        self.assertEqual(entity.attr_bases.get(name='attr-arr-obj').type, atype.AttrTypeArrObj)
+        self.assertFalse(entity.attr_bases.get(name='attr-str').is_mandatory)
+        self.assertTrue(entity.attr_bases.get(name='attr-obj').is_mandatory)
 
     def test_import_entity_with_unnecessary_param(self):
         user = self.admin_login()
@@ -76,22 +92,36 @@ class ImportTest(AironeViewTest):
 
     def test_import_entity_with_spoofing_user(self):
         admin = self.admin_login()
+        warning_messages = []
 
         # A user who creates original mock object
         user = User.objects.create(username='test-user')
 
-        Entity.objects.create(id=6, name='baz-original', created_user=user)
+        Entity.objects.create(id=3, name='baz-original', created_user=user)
         
         fp = self.open_fixture_file('entity.yaml')
-        resp = self.client.post(reverse('dashboard:do_import'), {'file': fp})
-        self.assertEqual(resp.status_code, 303)
+        with mock.patch('import_export.resources.logging') as lg_mock:
+            def side_effect(message):
+                warning_messages.append(str(message))
+
+            lg_mock.exception = mock.Mock(side_effect=side_effect)
+
+            resp = self.client.post(reverse('dashboard:do_import'), {'file': fp})
+            self.assertEqual(resp.status_code, 303)
         fp.close()
 
+        # checks to show warning messages
+        msg = r"failed to identify entity object"
+        self.assertEqual(len(warning_messages), 4)
+        self.assertTrue(all([re.match(msg, x) for x in warning_messages]))
+
         # checks that import data doens't appied
-        self.assertEqual(Entity.objects.get(id=6).name, 'baz-original')
+        entity = Entity.objects.get(id=3)
+        self.assertEqual(entity.name, 'baz-original')
 
         # checks that the AttributeBase objects which refers invalid Entity won't create
-        self.assertEqual(AttributeBase.objects.filter(name='attr4').count(), 0)
+        self.assertEqual(entity.attr_bases.count(), 0)
+        self.assertEqual(AttributeBase.objects.filter(name='attr-str').count(), 0)
 
     def test_import_entry(self):
         user = self.admin_login()
@@ -102,14 +132,23 @@ class ImportTest(AironeViewTest):
         fp.close()
 
         # checks that imported objects were normally created
-        self.assertEqual(Entry.objects.count(), 1)
-        self.assertEqual(Attribute.objects.count(), 3)
+        self.assertEqual(Entry.objects.count(), 5)
+        self.assertEqual(Attribute.objects.count(), 4)
 
         # checks that after_save_instance processing was normally worked
-        self.assertEqual(Entry.objects.last().attrs.count(), 3)
-        self.assertEqual(Entry.objects.last().attrs.get(name='aa').values.count(), 1)
-        self.assertEqual(Entry.objects.last().attrs.get(name='aa').type, 2)
+        entry = Entry.objects.get(name='entry1')
+        self.assertEqual(entry.attrs.count(), 4)
+        self.assertEqual(entry.attrs.get(name='attr-str').type, atype.AttrTypeStr)
+        self.assertEqual(entry.attrs.get(name='attr-obj').type, atype.AttrTypeObj)
+        self.assertEqual(entry.attrs.get(name='attr-arr-str').type, atype.AttrTypeArrStr)
+        self.assertEqual(entry.attrs.get(name='attr-arr-obj').type, atype.AttrTypeArrObj)
 
         # checks that a new AttribueValue was created by import-data
-        self.assertEqual(Attribute.objects.get(name='bb').values.count(), 2)
-        self.assertEqual(Attribute.objects.get(name='bb').type, 2)
+        self.assertEqual(Attribute.objects.get(name='attr-str').values.count(), 2)
+
+        # checks for the Array String attributes
+        attr = Attribute.objects.get(name='attr-arr-str')
+        self.assertEqual(attr.values.count(), 1)
+
+        attr_value = attr.values.last()
+        self.assertTrue(attr_value.status & AttributeValue.STATUS_DATA_ARRAY_PARENT)

63ae23b7226138817896e0d2a3376800b0222625
kobayashi-shu <kobayashi-shu@dmm.com>
2017-12-06 18:04:55 +0900

    Add user editing function

--- user/urls.py
+++ user/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^edit/(\d+)$', views.edit, name='edit'),
+    url(r'^do_edit/(\d+)$', views.do_edit, name='do_edit'),
     url(r'^create$', views.create, name='create'),
     url(r'^do_create$', views.do_create, name='do_create'),
     url(r'^do_delete$', views.do_delete, name='do_delete'),
--- user/views.py
+++ user/views.py
@@ -8,6 +8,7 @@ from airone.lib.http import HttpResponseSeeOther
 from airone.lib.http import http_get, http_post
 from airone.lib.http import render
 from airone.lib.http import check_superuser
+from airone.lib.http import _is_valid
 
 from .models import User
 
@@ -51,6 +52,63 @@ def do_create(request, recv_data):
 
     return render(request, 'create_user.html')
 
+@http_get
+@check_superuser
+def edit(request, user_id):
+
+    user = User.objects.get(id=user_id)
+
+    context = {
+        'user_id': int(user_id),
+        'user_name': user.username,
+        'user_email': user.email,
+        'user_password': user.password,
+        'user_is_superuser': user.is_superuser,
+    }
+
+    return render(request, 'edit_user.html', context)
+
+@http_post([
+    {'name': 'name', 'type': str, 'checker': lambda x: x['name']},
+    {'name': 'email', 'type': str, 'checker': lambda x: x['email']},
+])
+@check_superuser
+def do_edit(request, user_id, recv_data):
+
+    old_data = User.objects.get(id=user_id)
+    # validate duplication of username
+    if old_data.username != recv_data['name']:
+        if User.objects.filter(username=recv_data['name']).count():
+            return HttpResponse("username is duplicated", status=400)
+    # validate duplication of email
+    if old_data.email != recv_data['email']:
+        if User.objects.filter(email=recv_data['email']).count():
+            return HttpResponse("email is duplicated", status=400)
+
+    is_superuser = False
+    if 'is_superuser' in recv_data:
+        is_superuser = True
+
+    user = User(id=user_id,
+                username=recv_data['name'],
+                email=recv_data['email'],
+                is_superuser=is_superuser
+                )
+    user.save(update_fields=['username','email','is_superuser'])
+
+    # if new password was send, update password
+    if 'passwd' in recv_data:
+        # validate password format
+        validate =[{'name': 'passwd', 'type': str, 'checker': lambda x: x['passwd']}]
+        if not _is_valid(recv_data,validate):
+            return HttpResponse('Invalid parameters are specified', status=400)
+
+        # store encrypted password in the database
+        user.set_password(recv_data['passwd'])
+        user.save(update_fields=['password'])
+
+    return render(request, 'edit_user.html')
+
 @http_post([
     {'name': 'name', 'type': str, 'checker': lambda x: (
         x['name'] and (User.objects.filter(username=x['name']).count() == 1)
@@ -63,5 +121,5 @@ def do_delete(request, recv_data):
     # inactivate user
     user.delete()
 
-    # return empty response 
+    # return empty response
     return HttpResponse()

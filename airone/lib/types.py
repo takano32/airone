63a30d8a31a96d2764141b0f45355579eafaa423
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2018-01-11 02:11:08 +0000

    Updated entry view for adding new attr-type of named-entry

--- airone/lib/types.py
+++ airone/lib/types.py
@@ -4,8 +4,8 @@ _ATTR_OBJECT_TYPE = 1 << 0
 _ATTR_STRING_TYPE = 1 << 1
 _ATTR_TEXT_TYPE = 1 << 2
 _ATTR_BOOL_TYPE = 1 << 3
-_ATTR_NAMED_OBJECT_TYPE = 1 << 4
 _ATTR_ARRAY_TYPE = 1 << 10
+_ATTR_NAMED_TYPE = 1 << 11
 
 class MetaAttrType(type):
     def __eq__(cls, comp):
@@ -37,7 +37,7 @@ class AttrTypeStr(with_metaclass(MetaAttrType)):
 
 class AttrTypeNamedObj(with_metaclass(MetaAttrType)):
     NAME = 'named_entry'
-    TYPE = _ATTR_NAMED_OBJECT_TYPE
+    TYPE = _ATTR_OBJECT_TYPE | _ATTR_NAMED_TYPE
 
 class AttrTypeArrObj(with_metaclass(MetaAttrType)):
     NAME = 'array_entry'
@@ -49,7 +49,7 @@ class AttrTypeArrStr(with_metaclass(MetaAttrType)):
 
 class AttrTypeArrNamedObj(with_metaclass(MetaAttrType)):
     NAME = 'array_named_entry'
-    TYPE = _ATTR_NAMED_OBJECT_TYPE | _ATTR_ARRAY_TYPE
+    TYPE = _ATTR_OBJECT_TYPE | _ATTR_NAMED_TYPE | _ATTR_ARRAY_TYPE
 
 class AttrTypeText(with_metaclass(MetaAttrType)):
     NAME = 'textarea'

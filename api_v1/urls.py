98b528d3c4021dd2927b403d7c95e115abdbd777
Hiroyasu <user.localhost2000@gmail.com>
2018-07-26 05:44:46 +0000

    Implement a feature for user to show the backend processings that user executes

--- api_v1/urls.py
+++ api_v1/urls.py
@@ -4,10 +4,12 @@ from . import views
 from .user import views as user_views
 from .entity.urls import urlpatterns as entity_urlpatterns
 from .entry.urls import urlpatterns as entry_urlpatterns
+from .job.urls import urlpatterns as job_urlpatterns
 
 urlpatterns = [
     url(r'^entry$', views.EntryAPI.as_view()),
     url(r'^user/access_token$', user_views.AccessTokenAPI.as_view()),
     url(r'^entity/', include(entity_urlpatterns)),
     url(r'^entry/', include(entry_urlpatterns)),
+    url(r'^job/', include(job_urlpatterns)),
 ]

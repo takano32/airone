9e7f3187d8a815e667bf8c8ccc6f0d366c57c7bf
watanabe-k <watanabe-k@dmm.com>
2018-08-03 15:01:57 +0900

    first implement

--- group/views.py
+++ group/views.py
@@ -176,3 +176,80 @@ def export(request):
     output.write(UserResource().export().yaml)
 
     return get_download_response(output, 'user_group.yaml')
+
+@http_get
+def import_user_and_group(request):
+    return render(request, 'import_user_and_group.html', {})
+
+@http_file_upload
+@check_superuser
+def do_import_user_and_group(request):
+    if request.FILES['file'].size >= CONFIG.LIMIT_FILE_SIZE:
+        return HttpResponse("File size over", status=400)
+
+    try:
+        data = yaml.load(context)
+    except yaml.parser.ParserError:
+        return HttpResponse("Couldn't parse uploaded file", status=400)
+
+    # complement if empty in request
+    if 'Group' not in data:
+        data['Group'] = []
+
+    if 'User' not in data:
+        data['User'] = []
+
+    # update or create group
+    #
+    # because names of groups are needed when create or update user,
+    # groups should be update or create in advance
+    for group_data in data['Group']:
+        if 'name' not in group_data:
+            return HttpResponse("Group name is required", status=400)
+
+        if 'id' in group_data:
+            # update group
+            group = Group.objects.filter(id=group_data['id']).first()
+            if not group:
+                return HttpResponse("Specified id group does not exist(id:%s, group:%s)" %
+                                    (group_data['id'], group_data['name']), status=400)
+            group.name = group_data['name']
+            group.save()
+        else:
+            # create group
+            group = Group(name=group_data['name'])
+            group.save()
+
+    # update or create user
+    for user_data in data['User']:
+        for param in ['username', 'groups', 'email']:
+            if param not in user_data:
+                return HttpResponse("'%s' is required" % param, status=400)
+
+        user = None
+        if 'id' in user_data:
+            # update user by id when id is specified
+            user = User.objects.filter(id=user_data['id']).first()
+            if not user:
+                return HttpResponse("Specified id user does not exist(id:%s, user:%s)" %
+                                    (user_data['id'], user_data['username']), status=400)
+        else:
+            # update user by username
+            user = User.objects.filter(username=user_data['username']).first()
+            if not user:
+                # create user
+                user = User(username=user_data['username'])
+
+        user.username = user_data['username']
+        user.email = user_data['email']
+
+        new_groups = []
+        for group_name in user_data['groups'].split(","):
+            new_group = Group.objects.filter(name=group_name).first()
+            if not new_group:
+                return HttpResponse("Specified group does not exist(user:%s, group:%s)" %
+                                    (user_data['username'], group_name), status=400)
+            new_groups.append(new_group)
+
+        user.groups = new_groups
+        user.save()

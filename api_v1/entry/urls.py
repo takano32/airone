12ab710ac2c609b14dea3048ade7b16a3ef85536
Hiroyasu <user.localhost2000@gmail.com>
2018-08-17 06:06:19 +0000

    Added an API handler to get referral entries

--- api_v1/entry/urls.py
+++ api_v1/entry/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^search$', views.EntrySearchAPI.as_view()),
+    url(r'^referral$', views.EntryReferredAPI.as_view()),
 ]
--- api_v1/entry/views.py
+++ api_v1/entry/views.py
@@ -45,3 +45,29 @@ class EntrySearchAPI(APIView):
         resp = Entry.search_entries(user, hint_entity_ids, hint_attr, entry_limit)
 
         return Response({'result': resp}, content_type='application/json; charset=UTF-8')
+
+class EntryReferredAPI(APIView):
+    authentication_classes = (AironeTokenAuth, BasicAuthentication, SessionAuthentication,)
+
+    @airone_profile
+    def get(self, request):
+        user = User.objects.get(id=request.user.id)
+
+        param_entry = request.query_params.get('entry')
+        if not param_entry:
+            return Response({'result': 'Parameter "entry" is mandatory'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        ret_data = []
+        for entry in Entry.objects.filter(name=param_entry, is_active=True):
+            ret_data.append({
+                'id': entry.id,
+                'entity': {'id': entry.schema.id, 'name': entry.schema.name},
+                'referral': [{
+                    'id': x.id,
+                    'name': x.name,
+                    'entity': {'id': x.schema.id, 'name': x.schema.name},
+                } for x in entry.get_referred_objects()]
+            })
+
+        return Response({'result': ret_data}, content_type='application/json; charset=UTF-8')

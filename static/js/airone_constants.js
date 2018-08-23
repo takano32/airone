13a26ddca5a45cdd7cb2aa0cc4abf9e6fd78cd69
watanabe-k <watanabe-k@dmm.com>
2018-08-23 02:59:49 +0000

    implement gettext

--- static/js/airone_constants.js
+++ static/js/airone_constants.js
@@ -14,3 +14,31 @@ var LIMIT_FILE_SIZE = 10485760;
 
 // This phrase is used in the import alert
 var LIMIT_PHRASE = "※サイズが10Mを超えているのでUP不可※";
+
+
+// 
+var DICTIONARY = {
+  ja: {
+    button_communicating: '通信中...',
+    button_copy: 'コピー',
+    button_create: '作成',
+    button_edit: '編集',
+    button_save: '保存',
+    button_update: '更新',
+  },
+  en: { /* not implemented */ }
+};
+
+var DEFAULT_DICTIONARY = DICTIONARY['ja'];
+
+var setlang = function(lang) {
+  DEFAULT_DICTIONARY = DICTIONARY[lang];
+};
+
+var gettext = function(key, lang) {
+  if(lang) {
+    return DICTIONARY[lang][key];
+  }else {
+    return DEFAULT_DICTIONARY[key];
+  }
+};

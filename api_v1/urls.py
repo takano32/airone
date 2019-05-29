a8e9c2fe43c1d7a1252c94d4555b8cdafce83bd6
hasebe-teruaki <hasebe-teruaki@dmm.com>
2019-05-29 18:12:00 +0900

    Modified to support customview

--- api_v1/urls.py
+++ api_v1/urls.py
@@ -8,7 +8,6 @@ from .job.urls import urlpatterns as job_urlpatterns
 from airone.lib.log import Logger
 
 urlpatterns = [
-    url(r'^entry$', views.EntryAPI.as_view()),
     url(r'^user/access_token$', user_views.AccessTokenAPI.as_view()),
     url(r'^entity/', include(entity_urlpatterns)),
     url(r'^entry/', include(entry_urlpatterns)),
@@ -21,3 +20,9 @@ try:
     urlpatterns.append(url(r'^advanced/', include(custom_patterns)))
 except ImportError:
     Logger.info('advanced API endpoints are unavailable')
+
+try:
+    from custom_view.api_v1 import views as custom_views
+    urlpatterns.append(url(r'^entry$', custom_views.CustomEntryAPI.as_view()))
+except ImportError as e:
+    urlpatterns.append(url(r'^entry$', views.EntryAPI.as_view()))

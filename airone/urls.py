cc706b6ea2865031a0c97a00667d39c316869ef4
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2019-12-11 15:31:41 +0900

    Implemented a feature to be able to configure airone extension from changing of django settings

--- airone/urls.py
+++ airone/urls.py
@@ -1,18 +1,4 @@
-"""airone URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
+from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import RedirectView
@@ -32,3 +18,7 @@ urlpatterns = [
     url(r'^api/v1/', include(api_v1_urlpatterns)),
     url(r'^job/', include('job.urls', namespace='job')),
 ]
+
+for extension in settings.AIRONE['EXTENSIONS']:
+    urlpatterns.append(url(r'^extension/%s' % extension,
+                           include('%s.urls' % extension, namespace=extension)))
